8a72eaf7338f958642613c8e9770fb8e9811c54b
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-03-21 15:01:01 -0300

    fix(client): invert icon displaying in each theme

--- apps/client/src/components/theme-toggle.tsx
+++ apps/client/src/components/theme-toggle.tsx
@@ -16,8 +16,8 @@ export function ThemeToggle() {
             className="rounded-full p-5"
             onClick={() => {setTheme(theme === "light" ? "dark" : "light")}}
         >
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-300 ease-in-out dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-300 ease-in-out dark:rotate-0 dark:scale-100" />
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-300 ease-in-out dark:rotate-0 dark:scale-100" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-300 ease-in-out dark:-rotate-90 dark:scale-0" />
 
             <span className="sr-only">Toggle theme</span>
         </Button>
