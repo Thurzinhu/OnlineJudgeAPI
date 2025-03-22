d3b1f002b9c75a59fecc938a061575523f7eadce
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-03-22 12:19:33 -0300

    feat(client): add api get problems endpoint

---
+++ apps/client/src/lib/api.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { Problem } from "@/types/Problem";
+
+export async function getProblems(page: number, pageSize: number) {
+    const res = await fetch(`${process.env.WEB_URL}/problems?page=${page}&limit=${pageSize}`, {
+        cache: "force-cache",
+    });
+    const data: Problem[] = await res.json();
+
+    const totalCount = parseInt(res.headers.get('X-Total-Count') || '0');
+
+    return { data, totalCount };
+}
