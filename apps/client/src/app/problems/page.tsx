bde64e8ee8565a6bfb44617b84d8b7fff4022f29
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-03-22 19:58:45 -0300

    refactor(client): move margin bottom from navbar to margin top on main page

--- apps/client/src/app/problems/page.tsx
+++ apps/client/src/app/problems/page.tsx
@@ -17,7 +17,7 @@ export default async function ProblemsPage({ searchParams }: ProblemsPageProps) 
     const pageSize = 40;
 
     return (
-        <main className="mx-auto max-w-screen md:px-8 flex flex-col gap-4">
+        <main className="mx-auto max-w-screen md:px-8 flex flex-col gap-4 mt-12">
             <header className="flex justify-between items-center">
                 <h1 className="text-3xl font-medium tracking-wide">Problems</h1>
                 <ListLayout />
